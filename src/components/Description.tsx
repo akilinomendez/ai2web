bb556495c18946011e5db0466acf17402f2a0b01
akilinomendez <akilinomendez@gmail.com>
2023-02-07 14:56:28 +0000

    remove svg

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -10,26 +10,6 @@ export default function Description({ data }: { data: string }) {
           <p className="mx-auto mt-6  text-2xl leading-8 ">{data}</p>
         </div>
       </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1024 1024"
-        className="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
-        aria-hidden="true"
-      >
-        <circle
-          cx={512}
-          cy={512}
-          r={512}
-          fill="url(#radial)"
-          fillOpacity="0.7"
-        />
-        <defs>
-          <radialGradient id="radial">
-            <stop stopColor="#7775D6" />
-            <stop offset={1} stopColor="#E935C1" />
-          </radialGradient>
-        </defs>
-      </svg>
     </section>
   );
 }
